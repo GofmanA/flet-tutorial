bb8dd2ca533c454719f9d40f72d799b726ac9db1
l2700l <gofmanayu@gmail.com>
2024-11-20 00:58:13 +0400

    стилизация задач

--- main.py
+++ main.py
@@ -9,12 +9,27 @@ def main(page: ft.Page):
     def add_task(e):
         task_row = ft.Row(
             [
-                ft.Checkbox(label=task_input.value),
-                ft.IconButton(icon=ft.icons.DELETE, on_click=lambda event: remove_task(task_row)),
+                ft.Checkbox(
+                    label=task_input.value,
+                    on_change=lambda event: update_task(task_row),  # Функция для изменения цвета
+                ),
+                ft.IconButton(
+                    icon=ft.icons.DELETE,
+                    on_click=lambda event: remove_task(task_row),
+                ),
             ]
         )
         tasks.controls.append(task_row)
         task_input.value = ""  # Очистить поле
+        page.update()
+
+    def update_task(task_row: ft.Row):
+        checkbox: ft.Checkbox = task_row.controls[0]
+        if checkbox.value:  # Если задача выполнена
+            print(checkbox.label_style)
+            checkbox.label_style = ft.TextStyle(color=ft.colors.GREEN)
+        else:  # Если задача не выполнена
+            checkbox.label_style.color = None
         page.update()
 
     def remove_task(task_row):
