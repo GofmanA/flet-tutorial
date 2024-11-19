534adb67b40783a1bda883e1f7dba7c47fe7256c
l2700l <gofmanayu@gmail.com>
2024-11-20 00:33:14 +0400

    добавление отдельной колонки для задач

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Менеджер задач"
 
-    # Поле ввода
+    tasks = ft.Column()  # Список задач
     task_input = ft.TextField(label="Введите задачу")
-    
+
     def add_task(e):
-        page.add(ft.Text(task_input.value)) # У текстого поля берем значение
-                                            # и добавляем на страницу
+        tasks.controls.append(ft.Text(task_input.value)) # У текстого поля берем значение
+                                                         # и добавляем в колонку
         task_input.value = ""  # Очистить поле
         page.update()
 
-    page.add(task_input, ft.ElevatedButton("Добавить задачу", on_click=add_task))
+    page.add(task_input, ft.ElevatedButton("Добавить задачу", on_click=add_task), tasks)
 
 ft.app(target=main)
