9b92e21392b4454d74515f4600410e81f1687c07
l2700l <gofmanayu@gmail.com>
2024-11-20 00:32:01 +0400

    создание задач

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Менеджер задач"
-    page.add(ft.Text("Добро пожаловать в менеджер задач!"))
+
+    # Поле ввода
+    task_input = ft.TextField(label="Введите задачу")
+    
+    def add_task(e):
+        page.add(ft.Text(task_input.value)) # У текстого поля берем значение
+                                            # и добавляем на страницу
+        task_input.value = ""  # Очистить поле
+        page.update()
+
+    page.add(task_input, ft.ElevatedButton("Добавить задачу", on_click=add_task))
 
 ft.app(target=main)
